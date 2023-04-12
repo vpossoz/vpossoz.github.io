259ba969341bc1c58499718cb2d14cb8fcad7c40
vpossoz <vpos@hotmail.com>
2023-04-12 23:07:39 +0100

    try again

--- apiKey.js
+++ apiKey.js
@@ -1,3 +1,15 @@
- const apiKey = '1MxyAFcnbeMI9j00lqs0LfE5FkjreBup9sua7tj08';
- const appId = 'com.gther.valery_2epossoz.microverse';
+// const apiKey = "17k7J4fIGkblGk9oGtEL1G62d6gby6Kk47xuu4KAl";
+// const appId = "GtherMicroverse";
+// export default {apiKey, appId};
+
+ const apiKey = '1oz3IKM3dadcIHgipxkEmz1Adr14oJMEo0oC1ngeI';
+ const appId = 'vpossoz';
  export default  {apiKey, appId};
+
+ // For development on local computer and local network, use a development key
+// from croquet.io/keys and create a file called apiKey-dev.js.
+
+// For deployment to a publicly accessible server, create a production key
+// from croquet.io/keys and create a file called apiKey.js.
+
+// you may export other Croquet session parameters to override default values.
